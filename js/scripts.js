d9e05b9dbc6193281e69d3b9f2338fd123bfa6e1
Vladislav Khodkevich <whodkevich@gmail.com>
2026-01-30 16:06:18 +0300

    Move header and footer to separate files

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,33 @@
-var e = ("article,aside,figcaption,figure,footer,header,hgroup,nav,section,time").split(',');
+var e = ("main,article,aside,figcaption,figure,footer,header,hgroup,nav,section,time").split(',');
 for (var i = 0; i < e.length; i++) {
 	document.createElement(e[i]);
 }
+
+function setActiveMenuLink() {
+	const header = document.querySelector('header');
+	if (!header) return;
+
+	const links = header.querySelectorAll('nav a');
+	const path = location.pathname;
+
+	links.forEach(link => {
+		const href = link.getAttribute('href');
+
+		if (path.endsWith(href)) {
+			link.classList.add('active');
+		}
+
+		if (path.includes('/articles/') && href === 'articles.html') {
+			link.classList.add('active');
+		}
+	});
+}
+
+async function loadPart(id, file, callback) {
+	const res = await fetch(file);
+	document.getElementById(id).innerHTML = await res.text();
+	if (callback) callback();
+}
+
+loadPart('header', '/header.html', setActiveMenuLink);
+loadPart('footer', '/footer.html');
